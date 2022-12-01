c5cc41c111a7080954c1be42922c5c768eed18d1
Lenon Alcântara <lenonalcant@hotmail.com>
2022-12-01 14:05:14 -0300

    router

--- 06-react-router/router/src/0601-router/Home.js
+++ 06-react-router/router/src/0601-router/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+// import Head from './Head';
+import HelmetExport, { Helmet } from 'react-helmet';
+
 
 const Home = () => {
   return(
     <div>
+      {/* <Head title="Home" description="Essa é a descrição da home." /> */}
+      <Helmet>
+        <title>Dogs | Home</title>
+        <meta name="description" content="Essa é a descrição da Home." />
+      </Helmet>
       <h1>Home</h1>
       <p>Essa é a homepage</p>
       <Link to="produto/notebook">Notebook</Link>
